4e6273d35d3564589c33aafdcf10185e168bea70
Пользователь <noutkozimbek2023@gmail.com>
2024-05-11 22:16:45 +0500

    change settings.py

--- todoprj/settings.py
+++ todoprj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(i9(8hpcm_(=k$m!m)+^h-u90mwh4e#xcs86+zfp40b0@ztzkd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
